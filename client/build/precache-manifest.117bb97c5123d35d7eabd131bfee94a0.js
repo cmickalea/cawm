fb5a3371c3596f75e48210036bc6dbd68e94b465
Mickalea Chambers <iamchambers4@gmail.com>
2020-09-21 15:08:18 -0400

    add build to git

---
+++ client/build/precache-manifest.117bb97c5123d35d7eabd131bfee94a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe26680598858efdbe67818def42bef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf052dc6ae74435044b",
+    "url": "/static/css/main.33a4f585.chunk.css"
+  },
+  {
+    "revision": "e6e12614b817dfad577a",
+    "url": "/static/js/2.1c0fe581.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c0fe581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf052dc6ae74435044b",
+    "url": "/static/js/main.bd5fb322.chunk.js"
+  },
+  {
+    "revision": "ba13176e594acc9d2b86",
+    "url": "/static/js/runtime-main.89405878.js"
+  }
+]);
